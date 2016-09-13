7673ca877ff61dc880dbc18c8a672f6c533a9cba
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2016-09-13 20:53:14 +0300

    Fix CMHW compilation

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -16,8 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
-import java.io.File;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -33,11 +32,12 @@ public class KeyDisabler {
     private static String CONTROL_PATH = "/proc/touchscreen/nav_button_enable";
 
     public static boolean isSupported() {
-        return new File(CONTROL_PATH).exists();
+        return FileUtils.isFileReadable(CONTROL_PATH) &&
+            FileUtils.isFileWritable(CONTROL_PATH);
     }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return FileUtils.readOneLine(CONTROL_PATH).equals("0");
     }
 
     public static boolean setActive(boolean state) {
