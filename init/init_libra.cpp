828fa445f5937586f47f6636a5890ff5f530f4d4
Carlos Solano <csolano@gmail.com>
2016-12-23 08:05:32 +0100

    Init: Add product.name and cosmetic change
    Change-Id: I313e00ab83a56addd45ee6e83017df4ed12b0665

--- init/init_libra.cpp
+++ init/init_libra.cpp
@@ -121,14 +121,16 @@ static void init_target_properties()
 
     if (get_variant() == 0) {
         property_set("ro.build.product", "libra");
-        property_set("ro.product.model", "Mi-4c");
+        property_set("ro.product.model", "Mi 4c");
+        property_set("ro.product.name", "Mi 4c");
         property_set("ro.product.device", "libra");
         property_set("ro.build.description", "libra-user 6.0.1 LMY47V V8.0.5.0.LXKCNDG release-keys");
         property_set("ro.build.fingerprint", "Xiaomi/libra/libra:6.0.1/LMY47V/V8.0.5.0.LXKCNDG:user/release-keys");
         property_set("service.fingerprint.enable", "0");
     } else {
         property_set("ro.build.product", "aqua");
-        property_set("ro.product.model", "Mi-4s");
+        property_set("ro.product.model", "Mi 4s");
+        property_set("ro.product.name", "Mi 4s");
         property_set("ro.product.device", "aqua");
         property_set("ro.build.description", "aqua-user 6.0..1 LMY47V V8.0.2.0.LAJCNDG release-keys");
         property_set("ro.build.fingerprint", "Xiaomi/aqua/aqua:6.0.1/LMY47V/V8.0.2.0.LAJCNDG:user/release-keys");
