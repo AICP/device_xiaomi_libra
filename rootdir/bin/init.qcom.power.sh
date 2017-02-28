9000d40111c9e0d6b5213b0fa33d46444f45a2ed
Carlos Solano <csolano@gmail.com>
2017-02-28 07:48:12 +0100

    Revert "power: tweak values"
    This reverts commit e227ca0e0298e3653b8efee9e706ee8da2f1e30f.
    
    Change-Id: I4ea8d23c63db845faac9005eb13cab4d65fa87e4

--- rootdir/bin/init.qcom.power.sh
+++ rootdir/bin/init.qcom.power.sh
@@ -20,102 +20,135 @@ function get-set-forall() {
 
 ################################################################################
 
-# disable core control to switch governor
+# take the A57s offline when thermal hotplug is disabled
+write /sys/devices/system/cpu/cpu4/online 0
+write /sys/devices/system/cpu/cpu5/online 0
+
+# disable thermal bcl hotplug to switch governor
 write /sys/module/msm_thermal/core_control/enabled 0
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
+bcl_hotplug_mask=`get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_mask 0`
+bcl_hotplug_soc_mask=`get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask 0`
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
 
-# online main cores
-write /sys/devices/system/cpu/cpu0/online 1
-write /sys/devices/system/cpu/cpu4/online 1
-
-# restore after online
+# some files in /sys/devices/system/cpu are created after the restorecon of
+# /sys/. These files receive the default label "sysfs".
+# Restorecon again to give new files the correct label.
 restorecon -R /sys/devices/system/cpu
 
-# configure little cores interactive settings
+# Best effort limiting for first time boot if msm_performance module is absent
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
+
+# Limit A57 max freq from msm_perf module in case CPU 4 is offline
+write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000"
+
+# Disable CPU retention
+write /sys/module/lpm_levels/system/a53/cpu0/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu1/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu2/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a53/cpu3/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu4/retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/cpu5/retention/idle_enabled 0
+
+# Disable L2 retention
+write /sys/module/lpm_levels/system/a53/a53-l2-retention/idle_enabled 0
+write /sys/module/lpm_levels/system/a57/a57-l2-retention/idle_enabled 0
+
+# configure governor settings for little cluster
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
-restorecon -R /sys/devices/system/cpu # restore after interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 85
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 95
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 787200
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "85 960000:90 1248000:95"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460800:75 960000:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 39000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 79000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 1
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif 0
 
-# configure big cores interactive settings
+# online CPU4
+#write /sys/devices/system/cpu/cpu4/online 0
+#restorecon -R /sys/devices/system/cpu # must restore after online
+
+# configure governor settings for big cluster
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
-restorecon -R /sys/devices/system/cpu # restore after interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 20000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 85
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "85 1536000:90 1824000:95"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 39000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 79000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 1
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif 0
 
-# configure core_ctl for the big cores
-# minimum 0 cpus online, maximum 2 cores
-# cores are turned off at 60% load
-# and on at 60% load
-# cores will turn off after 100ms under 30% load
+# restore A57's max
+copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+
+# plugin remaining A57s
+#write /sys/devices/system/cpu/cpu5/online 1
+
+# Restore CPU 4 max freq from msm_performance
+write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:4294967295"
+
+# input boost configuration
+write /sys/module/cpu_boost/parameters/input_boost_freq "0:787200"
+write /sys/module/cpu_boost/parameters/input_boost_ms 40
+
+# Configure core_ctl
 write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 0
 write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 2
-write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 90
-write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 70
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 100
-write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
+write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
 
-# configure input boost
-# 20ms at 787200
-write /sys/module/cpu_boost/parameters/input_boost_enabled 1
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:787200"
-write /sys/module/cpu_boost/parameters/input_boost_ms 20
-
-# set scheduler parameters
-write /proc/sys/kernel/sched_boost 0
+# Setting B.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
-write /proc/sys/kernel/sched_upmigrate 95
-write /proc/sys/kernel/sched_downmigrate 90
+write /proc/sys/kernel/sched_small_task 30
+write /proc/sys/kernel/sched_upmigrate 99
+write /proc/sys/kernel/sched_downmigrate 85
 write /proc/sys/kernel/sched_freq_inc_notify 400000
 write /proc/sys/kernel/sched_freq_dec_notify 400000
-write /proc/sys/kernel/sched_spill_nr_run 3
-write /proc/sys/kernel/sched_init_task_load 100
 
-# offline big cores
-write /sys/devices/system/cpu/cpu4/online 0
-write /sys/devices/system/cpu/cpu5/online 1
+#enable rps static configuration
+write /sys/class/net/rmnet_ipa0/queues/rx-0/rps_cpus 8
 
-# enable core control
-write /sys/module/msm_thermal/core_control/enabled 1
-
-# android background processes are set to nice 10
-# don't schedule them on big cores
+# android background processes are set to nice 10. Never schedule these on the a57s.
 write /proc/sys/kernel/sched_upmigrate_min_nice 9
 
-# set gpu default power level to 5 instead of 4
+# devfreq
+get-set-forall /sys/class/devfreq/qcom,cpubw*/governor bw_hwmon
+restorecon -R /sys/class/devfreq/qcom,cpubw*
+get-set-forall /sys/class/devfreq/qcom,mincpubw*/governor cpufreq
+
+# Disable sched_boost
+write /proc/sys/kernel/sched_boost 0
+
+# re-enable thermal and BCL hotplug
+write /sys/module/msm_thermal/core_control/enabled 1
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode disable
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_mask $bcl_hotplug_mask
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask $bcl_hotplug_soc_mask
+get-set-forall /sys/devices/soc.0/qcom,bcl.*/mode enable
+
+# set GPU default power level to 5 (180MHz) instead of 4 (305MHz)
 write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5
-
-# set gpu default governor to msm-adreno-tz
-write /sys/class/devfreq/fdb00000.qcom,kgsl-3d0/governor msm-adreno-tz
-
-# enable all low power modes
-write /sys/module/lpm_levels/parameters/sleep_disabled 0
 
 # auo lcd color calibration
 if cat /sys/class/graphics/fb0/msm_fb_panel_info |grep -wq panel_name=auo
-then
+then   
  echo 253 225 256 > /sys/devices/platform/kcal_ctrl.0/kcal
  echo 1 > /sys/devices/platform/kcal_ctrl.0/kcal_enable
 fi
