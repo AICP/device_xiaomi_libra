4ce0fe2b994b90f50748ae6d17458e1093cd0a2a
Carlos Solano <csolano@gmail.com>
2017-09-16 15:08:20 +0200

    libra: fix dependencies and add aicp mk
    Change-Id: If623d8eeab46e91d490297b1486f73744c969ff1

---
+++ aicp.mk
@@ -0,0 +1,57 @@
+
+#
+# Copyright 2017 The AICP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from kenzo device
+$(call inherit-product, device/xiaomi/libra/device.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/aicp/config/telephony.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := libra
+PRODUCT_NAME := aicp_libra
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Mi4c
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Demon000 (Cosmin Tanislav) & csolanol (Carlos Solano)"
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/libra/libra:7.0/NRD90M/V8.2.3.0.NXKCNEC:user/release-keys" \
+    PRIVATE_BUILD_DESC="libra-user 7.0 NRD90M V8.2.3.0.NXKCNEC release-keys"
+
+
+TARGET_OTA_ASSERT_DEVICE := libra
