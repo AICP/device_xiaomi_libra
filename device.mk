add4514a540a74bc4c422052f8b4607b033468b6
Demon Singur <demonsingur@gmail.com>
2018-02-03 09:46:16 +0000

    configs: gps: import from bullhead
    Change-Id: I709f3b8193093191572ded4e1e419cbaae5456a7

--- device.mk
+++ device.mk
@@ -173,6 +173,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf
 
 PRODUCT_COPY_FILES += \
